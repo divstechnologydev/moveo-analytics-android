8734d77b561538825c199125e740db19d60ff257
Pavle Rogan <pavle@divstechnology.com>
2025-05-09 11:04:16 +0200

    app cleanup

--- simpleexample/src/main/java/one/moveo/simpleexample/SimpleAppApplication.java
+++ simpleexample/src/main/java/one/moveo/simpleexample/SimpleAppApplication.java
@@ -2,15 +2,11 @@ package one.moveo.simpleexample;
 
 import android.app.Application;
 
-import one.moveo.androidlib.MoveoOne;
 
 public class SimpleAppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        MoveoOne moveoOne = MoveoOne.getInstance();
-        moveoOne.setLogging(true);
-        moveoOne.initialize("YOUR_TOKEN_GOES_HERE");
-        moveoOne.start("context");
+
     }
 }
