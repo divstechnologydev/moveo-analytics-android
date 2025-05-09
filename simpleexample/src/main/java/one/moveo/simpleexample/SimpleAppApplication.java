b453b0f4eb26a09291f0e8f92c6113139a1d8ae5
MoveoOne Integration <integration@moveo.one>
2025-05-09 11:14:00 +0200

    MoveoOne automatic integration.

--- simpleexample/src/main/java/one/moveo/simpleexample/SimpleAppApplication.java
+++ simpleexample/src/main/java/one/moveo/simpleexample/SimpleAppApplication.java
@@ -3,10 +3,17 @@ package one.moveo.simpleexample;
 import android.app.Application;
 
 
+import one.moveo.androidlib.MoveoOne;
+
 public class SimpleAppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        // Initialize MoveoOne SDK
+        MoveoOne.getInstance().initialize("YOUR_SDK_TOKEN");
+        // (Optional) Identify user
+        // MoveoOne.getInstance().identify("USER_ID");
+        // Start tracking session (before first tick event)
+        MoveoOne.getInstance().start("main_activity_semantic");
     }
 }
